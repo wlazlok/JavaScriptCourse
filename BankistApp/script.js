4b4216d13ebe9628f5b48d4f74d2cbd8b12aca7b
wlazlok <kwlazo9@gmail.com>
2020-12-30 10:47:25 +0100

    add movments

--- BankistApp/script.js
+++ BankistApp/script.js
@@ -71,6 +71,22 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = ''; // delete static created movements
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  })
+}
+
+displayMovements(account1.movements);
+
 
 /////////////////////////////////////////////////
+
